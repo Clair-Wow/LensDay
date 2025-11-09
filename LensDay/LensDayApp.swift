de843b770e2ee0720dbbc91305594314f5de7167
Larry Clair <larryclair@outlook.com>
2025-11-09 10:24:49 -0500

    Initial Commit

---
+++ LensDay/LensDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LensDayApp.swift
+//  LensDay
+//
+//  Created by Larry Clair on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LensDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
